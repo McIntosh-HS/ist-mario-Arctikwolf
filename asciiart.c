3018069e0c5da87d2f98b9e992877615b1e12526
Penelope Klein <2027klein.penelope@fcboe.org>
2024-01-16 13:34:04 -0500

    made ascii art fox

---
+++ asciiart.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	
+	printf("    *      *\n");
+	printf("   *  *   *  *\n");
+	printf("  *  _    -  *\n");
+	printf("   * o    o *\n");
+	printf("    *      *\n");
+	printf("     *    *\n");
+	printf("    --*  *--\n");
+	printf("    ---*O*---\n");
+	return 0;
+}
